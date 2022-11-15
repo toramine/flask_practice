5afa6cb42e15b2b5fb60718eb29ce1e761e7c97c
TAIGA ISHIZUKA <>
2022-11-15 11:49:41 +0900

    edit, delete機能の追加、テンプレートの共通化と継承

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -14,7 +14,8 @@ crud = Blueprint(
 @crud.route("/")
 def index():
     memo_list = Memo.query.all()
-    return render_template("crud/index.html", memo_list=memo_list)
+    form = MemoForm()
+    return render_template("crud/index.html", memo_list=memo_list, form=form)
 
 
 @crud.route("/new", methods=["GET", "POST"])
@@ -26,3 +27,26 @@ def create_memo():
         db.session.commit()
         return redirect(url_for("crud.index"))
     return render_template("crud/create.html", form=form)
+
+
+@crud.route("/<memo_id>", methods=["GET", "POST"])
+def edit_memo(memo_id):
+    form = MemoForm()
+
+    memo = Memo.query.filter_by(id=memo_id).first()
+
+    if form.validate_on_submit():
+        memo.title = form.title.data
+        memo.text = form.text.data
+        db.session.add(memo)
+        db.session.commit()
+        return redirect(url_for("crud.index"))
+    return render_template("crud/edit.html", memo=memo, form=form)
+
+
+@crud.route("/<memo_id>/delete", methods=["POST"])
+def delete_memo(memo_id):
+    memo = Memo.query.filter_by(id=memo_id).first()
+    db.session.delete(memo)
+    db.session.commit()
+    return redirect(url_for("crud.index"))
