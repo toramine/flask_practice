536254d3c09dae0e9d2d6cdaafcdd9eb7139acd5
TAIGA <>
2022-11-19 12:02:28 +0900

    .envにSECRET_KEYの設定、SECRET_KEYの変更

--- apps/app.py
+++ apps/app.py
@@ -1,5 +1,7 @@
+import os
 from pathlib import Path
 
+from dotenv import load_dotenv
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -7,6 +9,7 @@ from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
+load_dotenv()
 
 
 def create_app():
@@ -14,10 +17,10 @@ def create_app():
     app.config["JSON_AS_ASCII"] = False
 
     app.config.from_mapping(
-        SECRET_KEY="3700c757-8e0d-4039-99f4-2530cbd82853",
+        SECRET_KEY=os.getenv("SECRET_KEY"),
         SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        WTF_CSRF_SECRET_KEY="5a38f877-3357-416c-b38d-eba99997b909",
+        WTF_CSRF_SECRET_KEY=os.getenv("WTF_CSRF_SECRET_KEY"),
     )
 
     db.init_app(app)
